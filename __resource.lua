beb17c4a20c99aaac3cba5832e30e847000d16d3
Yagiz ALP <yagizwasmeow@gmail.com>
2020-05-07 13:48:56 +0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,10 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+
+game 'gta5'
 
 description 'ESX Gym'
 
-version '0.0.2'
+version '0.0.3'
 
 server_scripts {
   '@es_extended/locale.lua',
